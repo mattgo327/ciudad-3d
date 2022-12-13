8594fb7b7b888318cdc1d0a961d336a03c84b338
Matias Garay <matiasgaray104@gmail.com>
2022-12-13 11:15:57 -0300

    servidor con json

--- servidor/recuperar.php
+++ servidor/recuperar.php
@@ -14,18 +14,25 @@ if ($conn->connect_error) {
     die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
   } 
 
-  echo 'Conectado a la base de datos.<br>';
+  //echo 'Conectado a la base de datos.<br>';
 
   $result = $conn->query("SELECT id, nombre FROM usuarios");
+  $rows= array();
+  while ($row=$result -> fetch_array()){
+    $rows[]=$row;
+  }
 
-  echo "Numero de resultados: $result->num_rows"."<br>";
+  $qryResult = array();
+  $qryResult['logs']=$rows;
+  echo json_encode($qryResult);
+  //echo "Numero de resultados: $result->num_rows"."<br>";
 
 
-  while ($fila = mysqli_fetch_array($result)) {
-    echo json_encode($fila);
-    echo "<br>";
-    }
-$result->close();
+  //while ($fila = mysqli_fetch_array($result)) {
+    //echo json_encode($fila);
+    //echo "<br>";
+    //}
+//$result->close();
   $conn->close();
 ?>
 </div>
