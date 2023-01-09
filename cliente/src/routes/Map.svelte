d3bd36d8c287749eedc41e0164e961906e1f4263
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2023-01-09 13:50:33 -0300

    Vuelve al punto antes del conflicto

--- cliente/src/routes/Map.svelte
+++ cliente/src/routes/Map.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { onMount, onDestroy, afterUpdate } from 'svelte';
+	import { onMount, afterUpdate } from 'svelte';
 	import { browser } from '$app/environment';
 	import { informacionParcela, section, layers } from '../store';
 	import { assets } from '$app/paths';
@@ -7,38 +7,8 @@
 	let mapElement;
 	let map;
 	let layersValue;
-
-	layers.subscribe((value) => {
-		layersValue = value;
-	});
-	// <test>
-
 	let select = false;
-
-	let layers = [
-		{
-			url: 'http://localhost:5173/layers/plazacity.geojson',
-			json: {},
-			mostrar: false
-		},
-		{
-			url: 'http://localhost:5173/layers/routes.geojson',
-			json: {},
-			mostrar: false
-		},
-		{
-			url: 'http://localhost:5173/layers/casa1.geojson',
-			json: {},
-			mostrar: false
-		},
-		{
-			url: 'http://localhost:5173/layers/casa2.geojson',
-			json: {},
-			mostrar: true
-		}
-	];
-
-	let a = {
+	const a = {
 		selected: {
 			color: 'red',
 			opacity: 100,
@@ -48,9 +18,9 @@
 		},
 		default: {
 			color: 'blue',
-			opacity: 0,
+			opacity: 1,
 			fillcolor: 'red',
-			fillOpacity: 0,
+			fillOpacity: 0.1,
 			weight: 0.5
 		}
 	};
@@ -58,12 +28,17 @@
 	let manyselect = 0;
 	let leaflet;
 	let layerGroup;
-	// </test>
+	let plazacityLayer;
+	let routesLayer;
+
+	layers.subscribe((value) => {
+		layersValue = value;
+	});
+
 	onMount(async () => {
 		if (browser) {
 			leaflet = await import('leaflet');
-
-			map = leaflet.map(mapElement).setView([-25.49765, -54.67885], 1700);
+			map = leaflet.map(mapElement).setView([-25.49765, -54.67885], 1700, 80);
 
 			leaflet
 				.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -72,105 +47,26 @@
 				})
 				.addTo(map);
 
-			/*test*/
-
 			layerGroup = new leaflet.LayerGroup();
 			layerGroup.addTo(map);
 
-
-			map.on('dblclick', function (e) {
-				var coord = e;
-				console.log(coord.latlng.lat + ' ' + coord.latlng.lng);
-			});
-
-			if (layersValue[0].mostrar) {
-				fetch(`${assets}/layers/plazacity.geojson`)
-					.then((response) => response.json())
-					.then((value) => {
-						let plazacityLayer = new leaflet.GeoJSON(value);
-						layerGroup.addLayer(plazacityLayer);
-						//polygonsBlock
-						leaflet
-							.geoJson(value, {
-								onEachFeature: popup,
-								style: a.default
-							})
-							.addTo(map);
-					});
-			}
-
-			if (layersValue[1].mostrar) {
-				fetch(`${assets}/layers/routes.geojson`)
-					.then((response) => response.json())
-					.then((value) => {
-						// Valor de la segunda capa
-						leaflet.geoJSON(value).addTo(map);
-						//polygonsBlock
-						leaflet
-							.geoJson(value, {
-								onEachFeature: popup,
-								style: a.default
-							})
-							.addTo(map);
-					});
-			}
-
-			fetch('http://localhost:5173/layers/casa1.geojson')
-				.then((response) => response.json())
-				.then((json) => {
-					layers[2].json = json;
-					if (is_active(2)) {
-						leaflet
-							.geoJSON(layers[2].json, {
-								onEachFeature: popup,
-								style: a.default
-							})
-							.addTo(map);
-						change_osmb(layers[2].url);
-					}
-				});
-
-			fetch('http://localhost:5173/layers/casa2.geojson')
-				.then((response) => response.json())
-				.then((json) => {
-					layers[3].json = json;
-					if (is_active(3)) {
-						leaflet
-							.geoJSON(layers[3].json, {
-								onEachFeature: popup,
-								style: a.default
-							})
-							.addTo(map);
-						change_osmb(layers[3].url);
-					}
-				});
-
-			/////////////////////// OSMB ////////////////////////
-			change_osmb();
+			const osmBuildings = (await import('osmbuildings/dist/OSMBuildings-Leaflet')).OSMBuildings;
+			new osmBuildings(map).load();
 		}
 	});
 
-	let plazacityLayer;
-	let routesLayer;
 	afterUpdate(async () => {
 		if (leaflet) {
 			if (layersValue[0].mostrar) {
 				if (!plazacityLayer) {
 					fetch(`${assets}/layers/plazacity.geojson`)
 						.then((response) => response.json())
-						.then((value) => {
-							plazacityLayer = new leaflet.GeoJSON(value, {
+						.then((data) => {
+							plazacityLayer = new leaflet.GeoJSON(data, {
 								onEachFeature: popup,
 								style: a.default
 							});
 							layerGroup.addLayer(plazacityLayer);
-							//polygonsBlock
-							// leaflet
-							// .geoJson(value, {
-							// 	onEachFeature: popup,
-							// 	style: a.default
-							// })
-							// 	.addTo(map);
 						});
 				} else if (!layerGroup.hasLayer(plazacityLayer)) {
 					layerGroup.addLayer(plazacityLayer);
@@ -188,13 +84,6 @@
 						.then((value) => {
 							routesLayer = new leaflet.GeoJSON(value);
 							layerGroup.addLayer(routesLayer);
-							//polygonsBlock
-							// leaflet
-							// 	.geoJson(value, {
-							// 		onEachFeature: popup,
-							// 		style: a.default
-							// 	})
-							// 	.addTo(map);
 						});
 				} else if (!layerGroup.hasLayer(routesLayer)) {
 					layerGroup.addLayer(routesLayer);
@@ -214,7 +103,7 @@
 				if (select && manyselect == 0) {
 					layer.setStyle(a.selected);
 					manyselect = 1;
-					to_do(e);
+					to_do(e, layer, select);
 				} else {
 					layer.setStyle(a.default);
 					manyselect = 0;
@@ -223,29 +112,10 @@
 		});
 	}
 
-	async function change_osmb(url_ = null) {
-		const osmBuildings = (await import('osmbuildings/dist/OSMBuildings-Leaflet')).OSMBuildings;
-		if (url_ == null) {
-			new osmBuildings(map).load();
-		} else {
-			new osmBuildings(map).load(url_);
-		}
-	}
-
-	onDestroy(async () => {
-		if (map) {
-			map.remove();
-		}
-	});
-
-	function to_do(ev) {
+	function to_do(event, layer, is_selected) {
 		section.set(1);
-		var feature = ev.target.feature;
+		var feature = event.target.feature;
 		informacionParcela.set(feature.properties);
-	}
-
-	function is_active(indice) {
-		return layers[indice].mostrar;
 	}
 </script>
 
